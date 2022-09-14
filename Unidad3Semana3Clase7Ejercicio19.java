4304f77cb37eb1ac1c7222f6552924fd5302b413
vkslhm <113329645+vkslhm@users.noreply.github.com>
2022-09-13 22:27:10 -0600

    Add files via upload

---
+++ Unidad3Semana3Clase7Ejercicio19.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    int i = 0;
+    while (true) {
+      i++;
+      System.out.println("Valor de i "+i);
+      if (i==9) { break; }
+    }
+  }
+}
