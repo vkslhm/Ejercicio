7e2fc1029821b4419c0b28cb5bba0c6afdb83b4a
vkslhm <113329645+vkslhm@users.noreply.github.com>
2022-09-13 19:28:30 -0600

    Add files via upload

---
+++ Unidad2Semana3Clase9Ejercicio12.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    for(int i=1; i<=10;i++){
+      System.out.println("********** Tabla del "+i+" *********");
+      for(int j=1; j<=10;j++) {
+      System.out.println(i+" multiplicado por "+j+" es igual a "+i*j);
+      }
+    }
+  }
+}
