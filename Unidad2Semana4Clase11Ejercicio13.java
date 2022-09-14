6663db6738ec823770a9612dfacdb940e564e546
vkslhm <113329645+vkslhm@users.noreply.github.com>
2022-09-13 19:47:45 -0600

    Add files via upload

---
+++ Unidad2Semana4Clase11Ejercicio13.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    String cadena= "";
+    for(int i=1; i<=20; i++) {
+      cadena = cadena + "<3 ";
+      System.out.println(cadena);
+    }
+  }
+}
