0b257d6ca0de81b960669780c4a7595656e44747
vkslhm <113329645+vkslhm@users.noreply.github.com>
2022-09-13 19:59:44 -0600

    Add files via upload

---
+++ Unidad3Semana4Clase4Ejercicio16.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    int i=0;
+    int j=10;
+    do {
+        i++;
+        j++;
+    }
+      while (i<=-1);
+      System.out.println(j);
+  }
+}
